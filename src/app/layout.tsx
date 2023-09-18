f3591f42663e675a719b64fc07a72040477503b6
franciscobarrerarodriguez <francisco.barrera@uptc.edu.co>
2023-09-18 11:19:29 -0500

    :alembic: removing bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 import type { Metadata } from 'next';
-import Script from 'next/script';
+import '@fontsource/inter';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,10 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} h-100`}>{children}</body>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-        crossOrigin="anonymous"></Script>
     </html>
   );
 }
