e6fde39867732c2f3f9d261ff7531f4550b66622
Jessica Perdomo Riano <jessica.riano@ADL00629.local>
2023-10-27 12:16:57 -0500

    :pencil: Updated aws config.

--- src/app/aws-exports.ts
+++ src/app/aws-exports.ts
@@ -1,13 +1,12 @@
 export default {
   region: 'us-east-2',
-  userPoolId: 'us-east-2_YCxsmE7NN',
-  userPoolWebClientId: '371e3j6v5i6iqrh7igkrkgb6d2',
+  userPoolId: 'us-east-2_uqGHWBI3L',
+  userPoolWebClientId: '52dbav00eord6jsro57ta4ii4h',
   oauth: {
-    domain: 'blockchain.auth.us-east-2.amazoncognito.com',
+    domain: 'blockchain2.auth.us-east-2.amazoncognito.com',
     redirectSignIn: 'http://localhost:4040/profile',
     redirectSignOut: 'http://localhost:4040/',
-    scope: ['email', 'openid'],
-    responseType: 'code'
+    responseType: 'token'
   },
   federationTarget: 'GOOGLE',
 };
