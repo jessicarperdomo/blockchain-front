8e8f350527144176c88cfd67c541eb8a77b8d464
Jessica Perdomo Riano <jessica.riano@adl00629.lan>
2023-11-10 20:10:39 -0500

    :pencil: Added redirect when profile hasnt token

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,81 +1,87 @@
 'use client';
 
-import MainNav from '@/app/components/main-nav/main-nav';
-import React from 'react';
-import Image from 'next/image';
-
-import './page.css';
+import { useEffect, useState } from 'react';
 import { Grid } from '@mui/joy';
+import { useRouter } from 'next/navigation';
+import MainNav from '@/app/components/main-nav/main-nav';
+import Image from 'next/image';
+import './page.css';
+import Web3 from 'web3';
 import url from 'url';
 
-import Web3 from 'web3';
 declare var window: any;
 
 export default function Profile() {
+  const currentUrl = window.location.href;
+
   const userDescription =
     'Descubre tu NFT y embarcate hacia el futuro de la tecnología e innovación explorando el mundo de la WEB3';
-  const avatar = '/avatar.png';
-  const avatar1 = '/avatar-1.svg';
-  const avatar2 = '/avatar-2.svg';
-  const avatar3 = '/avatar-3.svg';
 
-  const [loading, setLoading] = React.useState(true);
-  const [userToken, setUserToken] = React.useState('');
+  const [loading, setLoading] = useState(true);
+  const [userToken, setUserToken] = useState('');
+  const [accountId, setAccountId] = useState<string | null>(null);
+  const [accountValue, setAccountValue] = useState<string | null>(null);
+  const router = useRouter();
 
-  React.useEffect(() => {
+  useEffect(() => {
+    const getMetamaskAccountValue = async () => {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          await window.ethereum.enable();
+          const accounts = await web3.eth.getAccounts();
+          const account = accounts[0];
+          setAccountId(account);
+
+          const balance = await web3.eth.getBalance(account);
+          const balanceInEth = web3.utils.fromWei(balance, 'ether');
+
+          setAccountValue(balanceInEth);
+        } catch (error) {
+          console.error('Error obtaining Metamask account value:', error);
+        }
+      } else {
+        console.error('Metamask is not installed or not available');
+      }
+    };
+
+    getMetamaskAccountValue();
+  }, [setUserToken]);
+
+  useEffect(() => {
     if (!userToken) {
-      const currentUrl = window.location.href;
       const parsedUrl = url.parse(currentUrl, true);
+
       const accessToken = parsedUrl.hash && parsedUrl.hash.split('=')[1];
+
       if (accessToken) {
         setUserToken(accessToken);
         setLoading(false);
-        getMetamaskAccountValue();
+      } else {
+        handleRedirect();
       }
     }
   }, []);
 
-  async function getMetamaskAccountValue() {
-    if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
-      try {
-        await window.ethereum.enable();
+  const handleRedirect = () => {
+    router.push('/');
+  };
 
-        const accounts = await web3.eth.getAccounts();
-        const account = accounts[0];
-
-        const balance = await web3.eth.getBalance(account);
-
-        const balanceInEth = web3.utils.fromWei(balance, 'ether');
-
-        console.log('Valor de la cuenta de Metamask:', balanceInEth);
-      } catch (error) {
-        console.error(
-          'Error al obtener el valor de la cuenta de Metamask:',
-          error
-        );
-      }
-    } else {
-      console.error('Metamask no está instalado o no está disponible');
-    }
-  }
   const userDetails = (
-    <>
-      <div className="bc-profile-container">
-        <div>
-          <h1 className="bc-text-first-level">10K+</h1>
-          <p className="bc-text-second-level">usuarios</p>
-        </div>
-        <div>
-          <h1 className="bc-text-first-level">1.1M</h1>
-          <p className="bc-text-second-level">adl coins</p>
-        </div>
-        <div>
-          <h1 className="bc-text-first-level">38K+</h1>
-          <p className="bc-text-second-level">nfts</p>
-        </div>
+    <div className="bc-profile-container">
+      <div>
+        <h1 className="bc-text-first-level">10K+</h1>
+        <p className="bc-text-second-level">usuarios</p>
       </div>
-    </>
+      <div>
+        <h1 className="bc-text-first-level">{accountValue}</h1>
+        <p className="bc-text-second-level">adl coins</p>
+      </div>
+      <div>
+        <h1 className="bc-text-first-level">38K+</h1>
+        <p className="bc-text-second-level">nfts</p>
+      </div>
+    </div>
   );
 
   const userContainer = (
@@ -86,63 +92,44 @@ export default function Profile() {
   );
 
   const imagesContainer = (
-    <>
-      <Image
-        height={419}
-        width={419}
-        src={
-          loading
-            ? 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
-            : avatar
-        }
-        alt="avatar"
-      />
-    </>
+    <Image
+      height={419}
+      width={419}
+      src={loading ? '/placeholder.gif' : '/avatar.png'}
+      alt="avatar"
+    />
   );
 
   const avatarList = (
-    <>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center">
-        <Image
-          height={150}
-          width={120}
-          src={
-            loading
-              ? 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
-              : avatar1
-          }
-          alt="avatar"
-        />
-        <Image
-          height={150}
-          width={120}
-          src={
-            loading
-              ? 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
-              : avatar2
-          }
-          alt="avatar"
-        />
-        <Image
-          height={150}
-          width={120}
-          src={
-            loading
-              ? 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
-              : avatar3
-          }
-          alt="avatar"
-        />
-      </Grid>
-    </>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center">
+      <Image
+        height={150}
+        width={120}
+        src={loading ? '/placeholder.gif' : '/avatar-1.svg'}
+        alt="avatar"
+      />
+      <Image
+        height={150}
+        width={120}
+        src={loading ? '/placeholder.gif' : '/avatar-2.svg'}
+        alt="avatar"
+      />
+      <Image
+        height={150}
+        width={120}
+        src={loading ? '/placeholder.gif' : '/avatar-3.svg'}
+        alt="avatar"
+      />
+    </Grid>
   );
+
   return (
     <div>
-      <MainNav></MainNav>
+      <MainNav />
       <div className="bc-profile">
         <Grid
           alignItems="center"
